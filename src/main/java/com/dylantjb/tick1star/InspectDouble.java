70bb90aec1d4e1bb22b8df0bd244c13dffa30de5
Dylan Barker <dylanbarker59@gmail.com>
2020-11-03 12:20:38 +0000

    Completed tick 1 star and finished answers.txt

--- src/main/java/com/dylantjb/tick1star/InspectDouble.java
+++ src/main/java/com/dylantjb/tick1star/InspectDouble.java
@@ -2,28 +2,26 @@ package com.dylantjb.tick1star;
 
 public class InspectDouble {
     public static void main(String[] args) throws Exception {
-        double d = Double.parseDouble(args[0]);
+        double d = Double.parseDouble("100.0");
         // return the bits which represent the floating point number
         long bits = Double.doubleToLongBits(d);
 
         // Sign bit located in bit 63
         // Suggested Mask 0x8000000000000000L
         // Format 1 => number is negative
-        // TODO: fill in the XXXX
-        boolean negative = (XXXX) != 0;
+        boolean negative = (bits & 0x8000000000000000L) != 0;
 
         // Exponent located in bits 52 - 62
         // Suggested Mask 0x7ff0000000000000L
         // format Sum( 2^n * e(n)) - 1023 (binary number with bias)
-        // TODO: fill in the XXXX
-        long exponent = XXXX;
+        long exponent_bits = bits & 0x7ff0000000000000L;
+        int exponent = exponentToDecimal(exponent_bits);
+
 
         // Mantissa located in bits 0 - 51
         // Mask left as an exercise for the reader
         // format 1 + Sum(2^-(n+1) * m(n) )
-        // TODO: fill in the XXXX
-        long mantissa_bits = XXXX;
-
+        long mantissa_bits = bits & 0xfffffffffffffL;
         double mantissa = mantissaToDecimal(mantissa_bits);
 
         System.out.println((negative ? "-" : "") + mantissa + " x 2^" + exponent);
@@ -31,7 +29,12 @@ public class InspectDouble {
 
     private static double mantissaToDecimal(long mantissa_bits) {
         long one = 0x0010000000000000L;
-        return (double) (mantissabits + one) / (double) one;
+        return (double) (mantissa_bits + one) / (double) one;
+    }
+
+    private static int exponentToDecimal(long exponent_bits) {
+        long one = 0x0010000000000000L;
+        return (int) ((double)(exponent_bits) / (double)(one) - 1023);
     }
 }
 
